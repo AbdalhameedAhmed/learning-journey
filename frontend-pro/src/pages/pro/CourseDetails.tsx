97057c14f3ee8e584be150a2c789a6fbe203de48
Abdalhameed Ahmed <abdalhameed.med@gmail.com>
2025-09-29 18:50:20 +0300

    feat-frontend: Add next and prev functionality in the course details page

--- frontend-pro/src/pages/pro/CourseDetails.tsx
+++ frontend-pro/src/pages/pro/CourseDetails.tsx
@@ -7,9 +7,14 @@ import { useGetMe } from "@/hooks/auth/useGetMe";
 import { useGetCourseDetails } from "@/hooks/courseContent/useGetCourseDetails";
 import type { ExamHeader, LessonHeader } from "@schemas/course";
 import { ExamType } from "@schemas/Exam";
-import { Clipboard, HardDriveDownload, Heart } from "lucide-react";
+import { HardDriveDownload, Heart } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useParams, useSearchParams } from "react-router-dom";
+
+interface ModuleChild {
+  type: "lesson" | ExamType;
+  child: LessonHeader | ExamHeader;
+}
 
 export default function CourseDetails() {
   const courseId = useParams().courseId;
@@ -70,6 +75,127 @@ export default function CourseDetails() {
       setOpendModule(exam.module_id);
     } else {
       setOpendModule(undefined);
+    }
+  }
+
+  function getModuleIndexByExamId(examId: number) {
+    if (!courseDetails) return null;
+    const exam = courseDetails.exams.find((e) => e.id === examId);
+    if (exam && exam.module_id) {
+      return courseDetails.modules.findIndex((m) => m.id === exam.module_id);
+    }
+    return null;
+  }
+
+  function getLessonIndexAndModuleIndex(lessonId: number) {
+    let lessonIndex = null;
+    let moduleIndex = null;
+    courseDetails?.modules.forEach((module, mIndex) => {
+      module.lessons.forEach((lesson, lIndex) => {
+        if (lesson.id === lessonId) {
+          lessonIndex = lIndex;
+          moduleIndex = mIndex;
+        }
+      });
+    });
+    return { lessonIndex, moduleIndex };
+  }
+
+  function getNextModuleChild() {
+    const lessonId = searchParams.get("lessonId");
+    const examId = searchParams.get("examId");
+    if (lessonId) {
+      const { lessonIndex, moduleIndex } = getLessonIndexAndModuleIndex(
+        parseInt(lessonId),
+      );
+      if (lessonIndex === null || moduleIndex === null) return null;
+      if (
+        lessonIndex <
+        courseDetails!.modules[moduleIndex].lessons.length - 1
+      ) {
+        const nextChild =
+          courseDetails!.modules[moduleIndex].lessons[lessonIndex + 1];
+        return { type: "lesson", child: nextChild };
+      }
+      if (
+        lessonIndex ===
+        courseDetails!.modules[moduleIndex].lessons.length - 1
+      ) {
+        const nextChild = courseDetails!.modules[moduleIndex].quizzes[0];
+        return { type: ExamType.QUIZ, child: nextChild };
+      }
+    }
+
+    if (examId) {
+      const moduleIndex = getModuleIndexByExamId(parseInt(examId));
+      if (moduleIndex === null) return null;
+      if (moduleIndex < courseDetails!.modules.length - 1) {
+        const nextChild = courseDetails!.modules[moduleIndex + 1].lessons[0];
+        return { type: "lesson", child: nextChild };
+      }
+      if (moduleIndex === courseDetails!.modules.length - 1) {
+        const nextChild = courseDetails!.exams[0];
+        return { type: ExamType.FINAL_EXAM, child: nextChild };
+      }
+    }
+    return null;
+  }
+
+  function getPrevModuleChild(): ModuleChild | null {
+    const lessonId = searchParams.get("lessonId");
+    const examId = searchParams.get("examId");
+
+    if (lessonId) {
+      const { lessonIndex, moduleIndex } = getLessonIndexAndModuleIndex(
+        parseInt(lessonId),
+      );
+      if (lessonIndex === null || moduleIndex === null) return null;
+      if (lessonIndex == 0 && moduleIndex === 0) return null;
+
+      if (lessonIndex > 0) {
+        const prevChild =
+          courseDetails!.modules[moduleIndex].lessons[lessonIndex - 1];
+        return { type: "lesson", child: prevChild };
+      } else {
+        const prevModule = courseDetails!.modules[moduleIndex - 1];
+        const prevChild = prevModule.lessons[prevModule.lessons.length - 1];
+        return { type: "lesson", child: prevChild };
+      }
+    }
+    if (examId) {
+      const moduleId = getModuleIndexByExamId(parseInt(examId));
+      if (moduleId === null) return null;
+      const module = courseDetails!.modules[moduleId];
+      const prevChild = module.lessons[module.lessons.length - 1];
+      return { type: "lesson", child: prevChild };
+    }
+
+    return null;
+  }
+
+  function goToPrevModuleChild() {
+    const prevModuleChild = getPrevModuleChild();
+    if (!prevModuleChild) return;
+    if (prevModuleChild.type === "lesson") {
+      setActiveLessonHandler(prevModuleChild.child as LessonHeader);
+    } else {
+      setActiveExamHandler(
+        prevModuleChild.child as ExamHeader,
+        prevModuleChild.type,
+      );
+    }
+  }
+
+  function goToNextModuleChild() {
+    const nextModuleChild = getNextModuleChild();
+    if (!nextModuleChild) return;
+    if (nextModuleChild.type === "lesson") {
+      setActiveLessonHandler(nextModuleChild.child as LessonHeader);
+    } else {
+      setActiveExamHandler(
+        nextModuleChild.child as ExamHeader,
+        nextModuleChild.type as ExamType,
+      );
     }
   }
 
@@ -137,12 +263,23 @@ export default function CourseDetails() {
           {activeLesson && (
             <>
               <div className="flex w-full max-w-[800px] items-center justify-between">
-                <button>التالى</button>
+                <button
+                  className="bg-primary text-text-size cursor-pointer rounded-lg px-4 py-1 disabled:cursor-not-allowed disabled:opacity-50"
+                  disabled={!getNextModuleChild()}
+                  onClick={goToNextModuleChild}
+                >
+                  التالى
+                </button>
                 <div className="flex items-center justify-center gap-6">
-                  <button>السابق</button>
+                  <button
+                    className="bg-primary text-text-size cursor-pointer rounded-lg px-4 py-1 disabled:cursor-not-allowed disabled:opacity-50"
+                    disabled={!getPrevModuleChild()}
+                    onClick={goToPrevModuleChild}
+                  >
+                    السابق
+                  </button>
                   <div className="flex items-center gap-2">
                     <HardDriveDownload color="#3138A0" />
-                    <Clipboard color="#3138A0" />
                     <Heart color="#3138A0" />
                   </div>
                 </div>
